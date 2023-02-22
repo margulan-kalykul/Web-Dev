db323e378d1a5cda6e5dd48bd0df6265fd851189
pp2-21B030686 <m_kalykul@kbtu.kz>
2023-02-22 19:32:55 +0600

    Lab4: Online Store

---
+++ Lab4/shop/src/app/products/products.component.ts
@@ -0,0 +1,112 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
+})
+export class ProductsComponent {}
+
+export interface Product {
+  id: number;
+
+  image: string;
+  name: string;
+  description: string;
+  rating: string;
+
+  url: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Смартфон Apple iPhone 13 128Gb черный',
+    description: 'Description 1',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hc2/h05/46392662458398/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+    rating: '8/10',
+    url: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000',
+  },
+  {
+    id: 2,
+    name: 'Смартфон Apple iPhone 11 128Gb Slim Box черный',
+    description: 'Description 2',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h8b/h66/31489166606366/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg',
+    rating: '7/10',
+    url: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000',
+  },
+  {
+    id: 3,
+    name: 'Смартфон Apple iPhone 14 Pro Max 256Gb фиолетовый',
+    description: 'Description 3',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hab/hed/62948780671006/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+    rating: '9/10',
+    url: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000',
+  },
+  {
+    id: 4,
+    name: 'Xiaomi Redmi Note 11 Pro 8 ГБ/128 ГБ серый',
+    description: 'Description 4',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hd3/hfa/51166990303262/xiaomi-redmi-note-11-pro-8-gb-128-gb-seryi-103971386-1.jpg',
+    rating: '9/10',
+    url: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-11-pro-8-gb-128-gb-seryi-103971386/?c=750000000',
+  },
+  {
+    id: 5,
+    name: 'Xiaomi Redmi Note 10 Pro 8 ГБ/256 ГБ серый',
+    description: 'Description 5',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h30/hd2/64487157760030/xiaomi-redmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg',
+    rating: '9/10',
+    url: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000',
+  },
+  {
+    id: 6,
+    name: 'Apple iPhone 13 128Gb черный',
+    description: 'Description 6',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hc2/h05/46392662458398/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+    rating: '9/10',
+    url: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000',
+  },
+  {
+    id: 7,
+    name: 'Samsung Galaxy A23 6 ГБ/128 ГБ черный',
+    description: 'Description 7',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hfe/h14/49792685113374/smartfon-samsung-galaxy-a23-sm-a235fzkkskz-128gb-black-104348541-1.jpg',
+    rating: '9/10',
+    url: 'https://kaspi.kz/shop/p/samsung-galaxy-a23-6-gb-128-gb-chernyi-104348541/?c=750000000',
+  },
+  {
+    id: 8,
+    name: 'Samsung Galaxy A13 4 ГБ/128 ГБ черный',
+    description: 'Description 8',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h26/h48/49613711835166/samsung-galaxy-a13-4-gb-128-gb-chernyi-104253279-1.jpg',
+    rating: '9/10',
+    url: 'https://kaspi.kz/shop/p/samsung-galaxy-a13-4-gb-128-gb-chernyi-104253279/?c=750000000',
+  },
+  {
+    id: 9,
+    name: 'Apple iPhone 11 128Gb Slim Box черный',
+    description: 'Description 9',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h8b/h66/31489166606366/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg',
+    rating: '9/10',
+    url: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000',
+  },
+  {
+    id: 10,
+    name: 'Xiaomi Redmi 10C 4 ГБ/128 ГБ серый',
+    description: 'Description 10',
+    image:
+      'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hab/hed/62948780671006/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+    rating: '9/10',
+    url: 'https://kaspi.kz/shop/p/xiaomi-redmi-10c-4-gb-128-gb-seryi-104417231/?c=750000000',
+  },
+];
